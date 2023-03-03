59b3d50a3b802805a8f1636100c035f0ec009b9c
Alex <baklan4ik1993@gmail.com>
2023-03-03 01:46:33 +0100

    before fix

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes, useLocation } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
+import Home from "./screens/Home";
+import Model from "./screens/Model";
+import Photos from "./screens/Photos";
+
+import "./App.scss";
+import Header from "./components/Header";
+import Cursor from "./components/Cursor";
+import { useEffect, useState } from "react";
 
 function App() {
+  const location = useLocation();
+  const [cursorVariant, setCursorVariant] = useState("default");
+  
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
+  const imageDetails = {
+    width: 640,
+    height: 640,
+  };
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header setCursorVariant={setCursorVariant} />
+      <Cursor cursorVariant={cursorVariant} setCursorVariant={setCursorVariant} />
+
+      <AnimatePresence mode={ "wait"}>
+        <Routes location={ location} key={location.pathname}>
+          <Route
+            path="/"
+            element={<Home imageDetails={imageDetails} setCursorVariant={setCursorVariant} />}
+          />
+          <Route
+            path="/model"
+            element={<Model imageDetails={imageDetails} setCursorVariant={setCursorVariant} />}
+          />
+          <Route path="/beautifulMe" element={<Photos setCursorVariant={setCursorVariant} />} />
+        </Routes>
+      </AnimatePresence>
     </div>
   );
 }
